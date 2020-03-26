940d11833fc719a577a2635e7f3c898cee147356
cmatchneer <cmatchneer@yahoo.com>
2020-03-25 21:40:05 -0400

    more fixing

---
+++ build/precache-manifest.369d6013c0cc50b2c23bb6ba09f7e68b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1baabcae5cb23295bf5d594c0ec430c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dabc92df7193872fccb4",
+    "url": "/static/css/main.565cc41f.chunk.css"
+  },
+  {
+    "revision": "d1cba8154c3e2348f7f5",
+    "url": "/static/js/2.b74ac324.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b74ac324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabc92df7193872fccb4",
+    "url": "/static/js/main.fb75aeda.chunk.js"
+  },
+  {
+    "revision": "3592a8c51d9dd0f56b3e",
+    "url": "/static/js/runtime-main.609bff04.js"
+  },
+  {
+    "revision": "a9a6fd053d61355739e5294d3955740c",
+    "url": "/static/media/back.a9a6fd05.jpg"
+  },
+  {
+    "revision": "661ebda4be9679431b868ab3c5ce3022",
+    "url": "/static/media/golden_god.661ebda4.m4a"
+  },
+  {
+    "revision": "5167330df8a6cff8d59647c3435a7ae1",
+    "url": "/static/media/sign.5167330d.jpg"
+  }
+]);
