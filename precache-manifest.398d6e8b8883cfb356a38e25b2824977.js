2519e35d25c0d0015e30f731cec8c6e8fd709b9c
cmatchneer <cmatchneer@yahoo.com>
2020-03-26 20:06:07 -0400

    Updates

---
+++ precache-manifest.398d6e8b8883cfb356a38e25b2824977.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdf052a0b3964a1f0a3b78a3e879268",
+    "url": "/memory_game/index.html"
+  },
+  {
+    "revision": "0f893bd98e0fa31278a5",
+    "url": "/memory_game/static/css/main.565cc41f.chunk.css"
+  },
+  {
+    "revision": "3fb3c0e99e6e5bef56f3",
+    "url": "/memory_game/static/js/2.ca103dc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory_game/static/js/2.ca103dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f893bd98e0fa31278a5",
+    "url": "/memory_game/static/js/main.3a66f938.chunk.js"
+  },
+  {
+    "revision": "d10d69681aabef5df944",
+    "url": "/memory_game/static/js/runtime-main.91a731c6.js"
+  },
+  {
+    "revision": "a9a6fd053d61355739e5294d3955740c",
+    "url": "/memory_game/static/media/back.a9a6fd05.jpg"
+  },
+  {
+    "revision": "661ebda4be9679431b868ab3c5ce3022",
+    "url": "/memory_game/static/media/golden_god.661ebda4.m4a"
+  },
+  {
+    "revision": "5167330df8a6cff8d59647c3435a7ae1",
+    "url": "/memory_game/static/media/sign.5167330d.jpg"
+  }
+]);
